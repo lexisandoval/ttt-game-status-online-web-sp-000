1342411d624094f34af10e104f726bcedadc97f2
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-12 22:10:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,20 +32,4 @@ def won?(board)
       false
     end
   end
-
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[3]
-    win_index_3 = win_combination[6]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination # return the win_combination indexes that won.
-    else
-      false
-    end
-  end
 end
