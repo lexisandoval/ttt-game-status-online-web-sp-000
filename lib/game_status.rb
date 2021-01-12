3bbc9aebaaf3ec6d7a7891d30ab56990b13329d6
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-12 22:48:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,21 +17,32 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+  win_index1 = WIN_COMBINATIONS[0]
+  win_index2 = WIN_COMBINATIONS[1]
+  win_index3 = WIN_COMBINATIONS[2]
+  win_index4 = WIN_COMBINATIONS[3]
+  win_index5 = WIN_COMBINATIONS[4]
+  win_index6 = WIN_COMBINATIONS[5]
+  win_index7 = WIN_COMBINATIONS[6]
+  win_index8 = WIN_COMBINATIONS[7]
 
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-
-    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
-      return win_combination
-else
-  return nil
-    end
-
+  if (board[0] == "X" && board[1] == "X" && board[2] == "X") || (board[0] == "O" && board[1] == "O" && board[2] == "O")
+    return win_index1
+  elsif (board[3] == "X" && board[4] == "X" && board[5] == "X") || (board[3] == "O" && board[4] == "O" && board[5] == "O")
+    return win_index2
+  elsif (board[6] == "X" && board[7] == "X" && board[8] == "X") || (board[6] == "O" && board[7] == "O" && board[8] == "O")
+    return win_index3
+  elsif (board[0] == "X" && board[3] == "X" && board[6] == "X") || (board[0] == "O" && board[3] == "O" && board[6] == "O")
+    return win_index4
+  elsif (board[1] == "X" && board[4] == "X" && board[7] == "X") || (board[1] == "O" && board[4] == "O" && board[7] == "O")
+    return win_index5
+  elsif (board[2] == "X" && board[5] == "X" && board[8] == "X") || (board[2] == "O" && board[5] == "O" && board[8] == "O")
+    return win_index6
+  elsif (board[0] == "X" && board[4] == "X" && board[8] == "X") || (board[0] == "O" && board[4] == "O" && board[8] == "O")
+    return win_index7
+  elsif (board[2] == "X" && board[4] == "X" && board[6] == "X") || (board[2] == "O" && board[4] == "O" && board[6] == "O")
+    return win_index8
+  else
+    return nil
   end
 end
