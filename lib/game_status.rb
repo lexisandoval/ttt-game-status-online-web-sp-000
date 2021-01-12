466a836f4335f75fffcafe782391016a71df539b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-12 22:54:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,10 +20,10 @@ def won?(board)
   WIN_COMBINATIONS.each do |combinations|
     if (board[combinations[0]]== "X" && board[combinations[1]]=="X" && board[combinations[2]]=="X") ||
       (board[combinations[0]] == "O" && board[combinations[1]]== "O" && board[combinations[2]]== "O")
-    return combinations
+      return combinations
+    end
   end
-end
-return false
+  return false
 end
 
 def full?(board)
