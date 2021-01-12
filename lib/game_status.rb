366fa34af30d3b8c2520bd2b6d3fff13d830f562
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-12 21:40:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,12 +6,12 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  # [3,4,5],  # Middle row
-  # [6,7,8],  # Bottom row
-  # [0,3,6],  # Left col
-  # [1,4,7],  # Middle col
-  # [2,5,8],  # Right col
-  # [0,4,8],  # Diag 1
-  # [2,4,6]  # Diag 2
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left col
+  [1,4,7],  # Middle col
+  [2,5,8],  # Right col
+  [0,4,8],  # Diag 1
+  [2,4,6]  # Diag 2
   # ETC, an array for each win combination
 ]
